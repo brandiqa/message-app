65656aea6914bbb1be20fcaaa0c13ae84e8c69b1
Michael Wanyoike <micwanyoike@gmail.com>
2017-09-11 16:25:54 +0300

    Added functional syntax

--- src/messages/message-view.js
+++ src/messages/message-view.js
@@ -1,32 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class MessageView extends Component {
-  render() {
-
-    const message = this.props.message || {};
-
-    return(
-      <div className="container">
-        <div className="from">
-          <span className="label">From: </span>
-          <span className="value">{message.from}</span>
-        </div>
-        <div className="status">
-          <span className="label">Status: </span>
-          <span className="value">{message.status}</span>
-        </div>
-        <div className="message">
-          <span className="label">Message: </span>
-          <span className="value">{message.content}</span>
-        </div>
+export default function MessageView({message}) {
+  return (
+    <div className="container">
+      <div className="from">
+        <span className="label">From: </span>
+        <span className="value">{message.from}</span>
       </div>
-    )
-  }
+      <div className="status">
+        <span className="label">Status: </span>
+        <span className="value">{message.status}</span>
+      </div>
+      <div className="message">
+        <span className="label">Message: </span>
+        <span className="value">{message.content}</span>
+      </div>
+    </div>
+  );
 }
 
 MessageView.propTypes = {
   message: PropTypes.object.isRequired
 }
-
-export default MessageView;
